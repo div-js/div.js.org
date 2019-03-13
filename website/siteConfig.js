02cde5a7189a747b30e34b5749ffa5cc3a440459
smrsan76 <smrsan76@users.noreply.github.com>
2019-03-13 15:53:45 +0330

    Renamed projectName from div to div.js.org

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -19,7 +19,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "div",
+  projectName: "div.js.org",
   organizationName: "div-js",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
